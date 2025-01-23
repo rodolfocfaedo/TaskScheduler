680827fd6b98880be42cce41ee9f3bd31682e05e
Rodolfo <faedorodolfo.programming@gmail.com>
2025-01-23 13:14:15 -0300

    Save person method added

--- src/main/java/com/rodolfo/controller/PersonController.java
+++ src/main/java/com/rodolfo/controller/PersonController.java
@@ -1,9 +1,13 @@
 package com.rodolfo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodolfo.business.PersonService;
+import com.rodolfo.business.dto.PersonDTO;
 
 import lombok.RequiredArgsConstructor;
 
@@ -15,7 +19,10 @@ public class PersonController {
 	
 	private final PersonService personService;
 	
-	
+	@PostMapping
+	public ResponseEntity<PersonDTO> personSave(@RequestBody PersonDTO personDTO) {
+		return ResponseEntity.ok(personService.personSave(personDTO));
+	}
 	
 	
 	
